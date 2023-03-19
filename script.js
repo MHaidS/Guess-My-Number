f9e6c9ef62e98afb0fac3b14c91183cd3c63e04b
Reese-haids <reese.yes@gmail.com>
2023-03-19 22:27:33 +0800

    2nd commit

--- script.js
+++ script.js
@@ -9,23 +9,46 @@
 
 // console.log(document.querySelector('.guess').value); // NOTHING
 
-// document.querySelector('.guess').value = 23;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-// console.log(document.querySelector('.guess').value); // 23
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   // console.log(document.querySelector('.guess').value);
-
   // document.querySelector('.message').textContent = `Correct Number!`;
-
   const guess = Number(document.querySelector('.guess').value);
   // console.log(guess); // entered '2' in the input field & '2' was logged on the console when the 'Check!' btn was clicked
-
   // console.log(typeof guess); // string
-
   console.log(guess, typeof guess); // 2 'number'
 
   if (!guess) {
     document.querySelector('.message').textContent = '🚫 No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🥳 Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too High!';
+
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😢 You Lost the Game!';
+      score--;
+      document.querySelector('.score').textContent = score;
+      // .....OR.......
+      // document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😢 You Lost the Game!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
   }
 });
